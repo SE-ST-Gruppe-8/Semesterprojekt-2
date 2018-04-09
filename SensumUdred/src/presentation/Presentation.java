a33d931834eff3a30b1ec8816222c5055a6aa553
Robinlp <Robinlp@users.noreply.github.com>
2018-04-09 11:30:35 +0200

    removed main in presentation

--- SensumUdred/src/presentation/Presentation.java
+++ SensumUdred/src/presentation/Presentation.java
@@ -26,12 +26,4 @@ public class Presentation extends Application  {
         stage.setScene(scene);
         stage.show();
     }
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        launch(args);
-    }
-    
 }
