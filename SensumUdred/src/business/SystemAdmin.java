5485ac05bb81233e39860b589da291c91a0a71a6
polse123 <sorenbendtsen1993@gmail.com>
2018-04-18 13:13:52 +0200

    Revert "made constructors"
    This reverts commit 08191d7d2de2d074c0231a1fa3a708cb503a93b2.

--- SensumUdred/src/business/SystemAdmin.java
+++ SensumUdred/src/business/SystemAdmin.java
@@ -6,8 +6,4 @@ package business;
  */
 public class SystemAdmin extends User{
     
-    public SystemAdmin(String name, String ID, String userName, String password, String email) {
-        super(name, ID, userName, password, email);
-    }
-    
 }
