c1215c1b0b527c13b404cbb3df86631e21c31be7
niko6116 <niko6116@github.com>
2018-04-18 14:00:10 +0200

    SystemAdmin

--- SensumUdred/src/business/SystemAdmin.java
+++ SensumUdred/src/business/SystemAdmin.java
@@ -6,13 +6,13 @@ package business;
  */
 public class SystemAdmin extends User {
 
-
     public SystemAdmin(String name, String id, String userName, String password, String email) {
         super(name, id, userName, password, email);
     }
 
     public User createUser(String name, String id, String userName, String password, String email) {
-        User user = new User(name,id,userName,password,email) {};
+        User user = new User(name, id, userName, password, email) {
+        };
         return user;
     }
 
