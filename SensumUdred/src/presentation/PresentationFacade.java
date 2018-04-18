44c73fcddc99fc3d975ea46e898b3cc158ef890c
polse123 <sorenbendtsen1993@gmail.com>
2018-04-18 15:23:55 +0200

    s

--- SensumUdred/src/presentation/PresentationFacade.java
+++ SensumUdred/src/presentation/PresentationFacade.java
@@ -8,6 +8,7 @@ package presentation;
 import acq.*;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
