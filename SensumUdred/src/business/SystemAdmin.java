430f28b4af3db371e48dde8ebc80d932b875d6ec
frederiktt <32263257+frederiktt@users.noreply.github.com>
2018-04-18 13:20:35 +0200

    created SystemAdmin

--- SensumUdred/src/business/SystemAdmin.java
+++ SensumUdred/src/business/SystemAdmin.java
@@ -4,6 +4,20 @@ package business;
  *
  * @author Søren Bendtsen
  */
-public class SystemAdmin extends User{
-    
+public class SystemAdmin extends User {
+
+
+    public SystemAdmin(String name, String ID, String userName, String password, String email) {
+        super(name, ID, userName, password, email);
+    }
+
+    public void createUser() {
+
+        //Do stuff
+    }
+
+    public void deleteUser() {
+        //Do stuff
+    }
+
 }
