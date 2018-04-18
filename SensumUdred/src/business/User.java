bab8c117572dbeebe4e93f38678cc5d44e249a66
polse123 <sorenbendtsen1993@gmail.com>
2018-04-18 13:12:33 +0200

    user has been mode

--- SensumUdred/src/business/User.java
+++ SensumUdred/src/business/User.java
@@ -5,5 +5,54 @@ package business;
  * @author Søren Bendtsen
  */
 public abstract class User {
+
+    String name,ID,userName,password,email;
+
+    public User(String name, String ID, String userName, String password, String email) {
+        this.name = name;
+        this.ID = ID;
+        this.userName = userName;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+    
     
 }
