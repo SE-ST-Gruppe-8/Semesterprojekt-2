10f28336a38f3a64cb32117f30aa15f19c4aff50
niko6116 <niko6116@github.com>
2018-04-20 14:33:36 +0200

    Starter fix

--- SensumUdred/src/starter/Starter.java
+++ SensumUdred/src/starter/Starter.java
@@ -5,13 +5,18 @@ import acq.IData;
 import acq.IPresentation;
 import business.BusinessFacade;
 import data.DataFacade;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import presentation.PresentationFacade;
 
 /**
  *
  * @author Søren Bendtsen
  */
-public class Starter {
+public class Starter extends Application {
 
     /**
      * @param args the command line arguments
@@ -21,17 +26,26 @@ public class Starter {
         System.out.println("oof");
         IData data = new DataFacade();
         IBusiness business = new BusinessFacade();
-        System.out.println(business.getUsers());
-        IPresentation ui = new PresentationFacade();
         // Data is injected into business layer.
         business.injectData(data);
+        //System.out.println(business.getUsers());
+        IPresentation ui = new PresentationFacade();
         // Business is injecting into UI layer.
         ui.injectBusiness(business);
-        
+
         // Start GUI.
         ui.openUI();
     }
 
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+        stage.setTitle("Sensum Udred");
     }
 
-
+}
