24644e64a98d4e37565f46e75a5290117971f4b6
Robinlp <Robinlp@users.noreply.github.com>
2018-04-20 13:53:16 +0200

    data logger

---
+++ SensumUdred/src/data/DataLogger.java
@@ -0,0 +1,36 @@
+package data;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author Robin
+ */
+public class DataLogger {
+    
+    private PrintWriter output;
+    private String logPath = "Data.log";
+    
+    public void saveLog(String log){
+        try {
+            output = new PrintWriter(new FileWriter(logPath, true));
+            output.println(log);
+        } catch (IOException ex) {
+            System.out.println("could not log data");
+        }finally{
+            output.close();
+        }
+
+    }
+    
+//    public static void main(String[] args) {
+//        DataLogger dl = new DataLogger();
+//        dl.saveLog("test1");
+//        dl.saveLog("test2");
+//        dl.saveLog("test3");
+//        dl.saveLog("test4");
+//        
+//    }
+}
