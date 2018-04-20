36fbdc18ca62949831f7c1b7ddd8ec7a6c21d666
Robinlp <Robinlp@users.noreply.github.com>
2018-04-20 13:29:12 +0200

    log

--- SensumUdred/src/business/SecurityHandler.java
+++ SensumUdred/src/business/SecurityHandler.java
@@ -21,37 +21,39 @@ public class SecurityHandler {
     }
 
     public void logData(String dataToBeLogged) {
-        data.logData(activeUser.toString() + new Date().toString() + dataToBeLogged);
+        String log = activeUser.toString() + new Date().toString() + dataToBeLogged;
+        data.logData(log);
+        System.out.println("Logging: " + log);
     }
-    
-    public User getActiveUser(){
+
+    public User getActiveUser() {
         return activeUser;
     }
-    
-    public void setActiveUser(IUser user){
+
+    public void setActiveUser(IUser user) {
         switch (user.getRole()) {
             case 1:
-                this.activeUser = (SocialWorker)user;
+                this.activeUser = (SocialWorker) user;
                 break;
             case 0:
-                this.activeUser = (SystemAdmin)user;
+                this.activeUser = (SystemAdmin) user;
                 break;
             default:
                 System.out.println("error in user role");
                 break;
         }
-        
+
     }
-    
-    public void logOutActiveUser(){
+
+    public void logOutActiveUser() {
         this.activeUser = null;
     }
 
     public boolean validateUserLogin(ArrayList<IUser> users, String username, String password) {
         //if the login is true - find the user that matches the login info and set that user to activeUser
-        for(IUser u : users) {
-            if(u.getUsername().equals(username)) {
-                if(u.getPassword().equals(password)) {
+        for (IUser u : users) {
+            if (u.getUsername().equals(username)) {
+                if (u.getPassword().equals(password)) {
                     setActiveUser(u);
                     return true;
                 }
