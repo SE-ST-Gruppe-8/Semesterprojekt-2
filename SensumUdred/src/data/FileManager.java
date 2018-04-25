6848b3a0778c8ecfaef1626dd4fa2395d7c3f141
0xjb <Jonas_46@msn.com>
2018-04-25 13:34:13 +0200

    read & save files

--- SensumUdred/src/data/FileManager.java
+++ SensumUdred/src/data/FileManager.java
@@ -1,5 +1,6 @@
 package data;
 
+import acq.ICase;
 import acq.IUser;
 import business.SystemAdmin;
 import java.io.EOFException;
@@ -46,6 +47,30 @@ public class FileManager {
         return data;
     }
 
+    public ArrayList<ICase> readCases() {
+        file = new File("cases.dat");
+        ArrayList<ICase> data = new ArrayList<>();
+        boolean read = true;
+        try {
+            fileReader = new ObjectInputStream(new FileInputStream(file));
+            while (read) {
+                try {
+                    ICase c = (ICase) fileReader.readObject();
+                    data.add(c);
+                } catch (EOFException eof) {
+                    System.out.println("Reached end of file.");
+                    break; // stop reading
+                }
+            }
+
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Class not found.");
+        }
+        return data;
+    }
+
     public void writeToFile(ArrayList<IUser> data) {
         System.out.println(data);
         file = new File("users.dat");
@@ -59,7 +84,24 @@ public class FileManager {
             System.out.println("File not found.");
         } catch (IOException ex) {
             System.out.println("IOException encountered.");
-        }   
+        }
+
+    }
+
+    public void saveCases(ArrayList<ICase> data) {
+        System.out.println(data);
+        file = new File("cases.dat");
+        try {
+            fileWriter = new ObjectOutputStream(new FileOutputStream(file));
+            for (ICase c : data) {
+                fileWriter.writeObject(c);
+            }
+            fileWriter.close();
+        } catch (FileNotFoundException ex) {
+            System.out.println("File not found.");
+        } catch (IOException ex) {
+            System.out.println("IOException encountered.");
+        }
 
     }
 
@@ -67,7 +109,6 @@ public class FileManager {
         ArrayList<IUser> test = new ArrayList<>();
         FileManager fm = new FileManager();
         test.add(new SystemAdmin("ASS", "b", "starts", "twerking", "e"));
-
 
         fm.writeToFile(test);
         System.out.println(fm.readFile());
