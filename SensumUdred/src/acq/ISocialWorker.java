56d8268ab3ed79262321a35abd3d1977e3a4cdfd
Robinlp <Robinlp@users.noreply.github.com>
2018-04-26 09:25:02 +0200

    ISocialWorker

---
+++ SensumUdred/src/acq/ISocialWorker.java
@@ -0,0 +1,43 @@
+package acq;
+
+import business.Case;
+import business.Citizen;
+import business.Inquiry;
+import business.Reference;
+import business.SocialWorker;
+import java.util.Set;
+import javafx.collections.ObservableList;
+
+/**
+ *
+ * @author Robin
+ */
+public interface ISocialWorker {
+    public ICase createCase(String id, String des, String process, SocialWorker sw, Citizen c);
+
+    public boolean deleteCase(ICase newCase, ObservableList<ICase> cases);
+
+    public boolean createReference(String id, String socialInstance, String description);
+
+    public boolean informCitizen(Case c);
+
+    public Set<Case> getCases();
+
+    public Set<Inquiry> getInquiries();
+
+    public Set<Reference> getReferences();
+
+    public boolean addCase(Case c);
+
+    public boolean addInquiry(Inquiry inq);
+
+    public boolean addReference(Reference ref);
+
+    public boolean removeCase(Case c);
+
+    public boolean removeInquiry(Inquiry inq);
+
+    public boolean removeReference(Reference ref);
+
+    public int getRole();
+}
