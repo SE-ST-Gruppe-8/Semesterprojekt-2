0247fe2d61fe51618d6cf1999de3b6a6a30c672a
niko6116 <niko6116@github.com>
2018-05-17 11:49:49 +0200

    database SQL update

--- SensumUdred/src/data/DatabaseSQL.sql
+++ SensumUdred/src/data/DatabaseSQL.sql
@@ -15,14 +15,14 @@ CREATE TABLE Citizens (
 );
 
 CREATE TABLE Cases (
-    id CHAR(10) PRIMARY KEY,
-    description VARCHAR(200),
+    caseId CHAR(13) PRIMARY KEY,
+    caseDescription VARCHAR(200),
     process VARCHAR(200)
 );
 
 CREATE TABLE Inquiries (
-    id CHAR(10) PRIMARY KEY,
-    description VARCHAR(200),
+    caseId CHAR(13) PRIMARY KEY,
+    caseDescription VARCHAR(200),
     isCitizenInformed Boolean,
     origin VARCHAR(200)
 );
@@ -43,21 +43,21 @@ CREATE TABLE RegisteredBy (
 
 CREATE TABLE HasCase (
     citizenId CHAR(10),
-    caseId CHAR(10),
+    caseId CHAR(13),
     FOREIGN KEY (citizenId) REFERENCES Citizens(id) ON DELETE SET NULL ON UPDATE CASCADE,
     FOREIGN KEY (caseId) REFERENCES Cases(id) ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 CREATE TABLE HasInquiry (
     citizenId CHAR(10),
-    inquiryId CHAR(10),
+    inquiryId CHAR(13),
     FOREIGN KEY (citizenId) REFERENCES Citizens(id) ON DELETE SET NULL ON UPDATE CASCADE,
     FOREIGN KEY (inquiryId) REFERENCES Inquiries(id) ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 CREATE TABLE CreatedBy (
     userId CHAR(10),
-    caseId CHAR(10),
+    caseId CHAR(13),
     FOREIGN KEY (userId) REFERENCES Users(id) ON DELETE SET NULL ON UPDATE CASCADE,
     FOREIGN KEY (caseId) REFERENCES Cases(id) ON DELETE SET NULL ON UPDATE CASCADE
 );
